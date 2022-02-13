315bfcfd2667eccf91f7d6f176b455148ee41f1c
Leandro <leandro.khiro@gmail.com>
2022-02-13 17:08:29 -0300

    Apply style to project

--- src/App.vue
+++ src/App.vue
@@ -17,22 +17,107 @@
 </script>
 
 <template>
-  <form @submit.prevent="addTodo">
-    <label for="newTodo"></label>
-    <input type="text" name="newTodo" v-model="newTodo">
+  <section class="lateral-menu">
+    <header>
+      <h1 class="app-title">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-list"><line x1="8" y1="6" x2="21" y2="6"></line><line x1="8" y1="12" x2="21" y2="12"></line><line x1="8" y1="18" x2="21" y2="18"></line><line x1="3" y1="6" x2="3.01" y2="6"></line><line x1="3" y1="12" x2="3.01" y2="12"></line><line x1="3" y1="18" x2="3.01" y2="18"></line></svg>
 
-    <button type="submit">Add todo</button>
-  </form>
+        to do
+      </h1>
+    </header>
 
-  <ul>
-    <li v-for="(todo, index) in todos" :key="index">
-      <span :class="todo.isComplete ? 'completed' : ''">{{ todo.text }}</span>
-      <button @click="removeTodo(index)">Remove</button>
-      <button @click="toggleCompleteTodo(index)">{{ todo.isComplete ? 'Uncompleted' : 'Completed' }}</button>
-    </li>
-  </ul>
+    <form @submit.prevent="addTodo">
+      <div class="form-group">
+        <label for="newTodo">New to do</label>
+
+        <div class="input-group">
+          <input type="text" id="newTodo" v-model="newTodo">
+
+          <button type="submit" class="btn-success">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+          </button>
+        </div>
+      </div>
+    </form>
+  </section>
+
+  <main>
+    <section class="container">
+      <ul class="todo-list">
+        <li v-for="(todo, index) in todos" :key="index">
+          <input
+            type="checkbox"
+            :id="`doneCheck${index}`"
+            @input="toggleCompleteTodo(index)"
+          >
+
+          <label :for="`doneCheck${index}`" :class="todo.isComplete ? 'completed' : ''">{{ todo.text }}</label>
+
+          <button
+            class="btn btn-danger remove"
+            @click="removeTodo(index)"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
+          </button>
+        </li>
+      </ul>
+    </section>
+  </main>
 </template>
 
 <style>
-  .completed { text-decoration: line-through; }
+  .container { padding: 1rem; }
+
+  .lateral-menu {
+    background-color: white;
+    box-shadow:       1px 0 #e7e7e7;
+    padding:          2rem;
+  }
+
+  .app-title {
+    align-items:    center;
+    display:        flex;
+    gap:            .5em;
+    margin-bottom:  3rem;
+  }
+
+  .feather-plus {
+    height: 1rem;
+  }
+
+  .todo-list {
+    display:        flex;
+    flex-direction: column;
+    gap:            .8rem;
+    padding:        0;
+  }
+
+  .todo-list li {
+    align-items:  center;
+    display:      flex;
+    padding:      .2em;
+  }
+
+  .todo-list li:hover {
+    background-color: #e9f1fb;
+    border-radius:        .4em;
+  }
+
+  .todo-list li label {
+    cursor:       pointer;
+    flex:         1 1 auto;
+    padding-left: .6em;
+    padding-right: 1.2em;
+
+    transition: color .2s;
+  }
+
+  .todo-list li .remove svg {
+    height: 1rem;
+  }
+
+  .completed {
+    color:        #7e7d8d;
+    text-decoration:  line-through;
+  }
 </style>
